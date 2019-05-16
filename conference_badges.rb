9ea47c1711fe8758d0c950858f7ff969d7567339
Trish Ogen <patriciaogen@gmail.com>
2019-05-16 23:41:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,3 +5,8 @@ end
 def batch_badge_creator(attendees)
   attendees.collect{|name| badge_maker(name)}
 end
+
+def assign_rooms(speakers)
+  speakers.collect{|speaker| "Hello, #{speaker}! You'll be assigned to 
+  room #{a.find_index(speaker)}!"}
+end
