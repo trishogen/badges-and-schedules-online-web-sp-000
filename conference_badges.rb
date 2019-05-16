1428582f761dac66c68a97c529ccf4ecaca35903
Trish Ogen <patriciaogen@gmail.com>
2019-05-16 23:52:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,6 +9,7 @@ end
 def assign_rooms(speakers)
   speakers.each_with_index do |speaker, index|
     "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"}
+  end
 end
 
 def printer(attendees)
